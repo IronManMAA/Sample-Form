8188d7e569f60cbbf2dd24aed43f7dc88f2eca70
Marco Almeida <marcoalmeida@Marcos-MacBook-Pro.local>
2016-12-06 15:23:37 -0500

    validation

--- Sample Form/StowberryTableViewController.m
+++ Sample Form/StowberryTableViewController.m
@@ -45,58 +45,137 @@
 {
     BOOL returnValue =NO;
     [self.view endEditing:YES];
-    if (textField==self.NameTextField)
-        // if the textfield in question is the bandNameTextField
-    {
+    NSCharacterSet *setN = [NSCharacterSet characterSetWithCharactersInString:@"0123456789"];
+    NSCharacterSet *setL = [NSCharacterSet characterSetWithCharactersInString:@"AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz"];
     // do validation here
-        if ([self.NameTextField.text isEqualToString:@""]) {
-            //if field is empty
-            self.title  = @"Please provide full name";
-            [self.NameTextField becomeFirstResponder];
-//            self.NameTextField.textColor = [UIColor whiteColor];
-//            self.NameTextField.backgroundColor = [UIColor redColor];
-//            self.NameTextField.text  = @"Please provide full name";
+    if (textField==self.NameTextField) {
+        if ([self.NameTextField.text isEqualToString:@""])
+        {self.title  = @"Please provide you full name"; [self.NameTextField becomeFirstResponder];
         } else {
-            [self.AddressTextField becomeFirstResponder];
-            // [self.bandNameTextField becomeFirstResponder];
-            // becomeFirstResponder tells the system to place cursor in the field given name here
-            // Use this to move to the next field
+            NSArray *fullName = [self.NameTextField.text componentsSeparatedByString:@" "];
+            if (fullName.count < 2)
+            {
+                self.title  = @"Please provide your First and Last Name.";
+                [self.NameTextField becomeFirstResponder];
+            } else {
+            
+               NSString *dataProvided = self.NameTextField.text;
+               if ([dataProvided rangeOfCharacterFromSet:setN].location != NSNotFound)
+               {
+                   self.title  = @"Please provide your full Name. Numbers are not allowed";
+                   [self.NameTextField becomeFirstResponder];
+               } else {
+                   returnValue = YES;
+                   [self.AddressTextField becomeFirstResponder];
+                   self.title  = @"";
+               }
+            }
         }
     }
+    
     if (textField==self.AddressTextField) {
         if ([self.AddressTextField.text isEqualToString:@""])
            {self.title  = @"Please provide Street Address"; [self.AddressTextField becomeFirstResponder];
-        } else { [self.CityTextField becomeFirstResponder];}
+        } else { [self.CityTextField becomeFirstResponder];
+            self.title  = @"";
+        }
     }
     if (textField==self.CityTextField) {
         if ([self.CityTextField.text isEqualToString:@""])
         {self.title  = @"Please provide City"; [self.CityTextField becomeFirstResponder];
-        } else { [self.StateTextField becomeFirstResponder];}
+        } else {
+                NSString *dataProvided = self.CityTextField.text;
+                if ([dataProvided rangeOfCharacterFromSet:setN].location != NSNotFound)
+                {
+                    self.title  = @"Please provide City. Numbers are not allowed";
+                    [self.CityTextField becomeFirstResponder];
+                } else {
+                    returnValue = YES;
+                    [self.StateTextField becomeFirstResponder];
+                    self.title  = @"";
+                }
+        }
     }
     if (textField==self.StateTextField) {
         if ([self.StateTextField.text isEqualToString:@""])
         {self.title  = @"Please provide State"; [self.StateTextField becomeFirstResponder];
-        } else { [self.ZipTextField becomeFirstResponder];}
+        } else {
+            NSString *dataProvided = self.StateTextField.text;
+            if ([dataProvided rangeOfCharacterFromSet:setN].location != NSNotFound)
+            {
+                self.title  = @"Please provide State. Numbers are not allowed";
+                [self.StateTextField becomeFirstResponder];
+            } else {
+                returnValue = YES;
+                [self.ZipTextField becomeFirstResponder];
+                self.title  = @"";
+            }
+        }
     }
     if (textField==self.ZipTextField) {
         if ([self.ZipTextField.text isEqualToString:@""])
         {self.title  = @"Please provide Zip Code"; [self.ZipTextField becomeFirstResponder];
-        } else { [self.PhoneTextField becomeFirstResponder];}
-    }
-    if (textField==self.PhoneTextField) {
-        if ([self.PhoneTextField.text isEqualToString:@""])
-            {self.title  = @"Please provide Phone Number"; [self.PhoneTextField becomeFirstResponder];
         } else {
- //           [self.PhoneTextField becomeFirstResponder];
-        self.title  = @"Job Completed. Thank You!";
-        [self.view endEditing:NO];
+            NSString *dataProvided = self.ZipTextField.text;
+            if ([dataProvided rangeOfCharacterFromSet:setL].location != NSNotFound)
+            {
+                self.title  = @"Please provide Zip Code. Letters are not allowed";
+                [self.ZipTextField becomeFirstResponder];
+            } else {
+                returnValue = YES;
+                [self.PhoneTextField becomeFirstResponder];
+                self.title  = @"";
+            }
         }
     }
-
+    
+    if (textField==self.PhoneTextField) {
+        if ([self.PhoneTextField.text isEqualToString:@""])
+        {self.title  = @"Please provide Phone Number"; [self.PhoneTextField becomeFirstResponder];
+        } else {
+            NSString *dataProvided = self.PhoneTextField.text;
+            if ([dataProvided rangeOfCharacterFromSet:setL].location != NSNotFound)
+            {
+                self.title  = @"Please provide Phone number. Letters are not allowed";
+                [self.PhoneTextField becomeFirstResponder];
+            } else {
+                returnValue = YES;
+                self.title  = @"Job Completed. Thank You!";
+                [self.view endEditing:NO];
+            }
+        }
+    }
     return returnValue;
 }
 
+// Sample Validation code 
+/*
+else if (textField == self.streetTextField)
+{
+    if (![self.streetTextField.text isEqualToString:@""])
+    {
+        NSArray *streetComponents = [self.streetTextField.text componentsSeparatedByString:@" "];
+        if (streetComponents.count == 3)
+        {
+            NSString *streetNumber = streetComponents[0];
+            NSString *streetType = streetComponents[2];
+            
+            NSCharacterSet *set = [NSCharacterSet characterSetWithCharactersInString:@"0123456789"];
+            if ([streetNumber rangeOfCharacterFromSet:set].location != NSNotFound)
+            {
+                NSCharacterSet *set = [NSCharacterSet characterSetWithCharactersInString:@"AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz"];
+                if ([streetType rangeOfCharacterFromSet:set].location != NSNotFound)
+                {
+                    returnValue = YES;
+                    [self.cityTextField becomeFirstResponder];
+                }
+            }
+        }
+    }
+    
+}
 
+*/
 
 #pragma mark - Table view data source
 
